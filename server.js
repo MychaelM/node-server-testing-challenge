cc472bbecaf4fc29001bbb69e13762f2a5b5f6ec
Mychael M <mychaelbmenges@gmail.com>
2021-01-21 15:26:07 -0700

    added test for server welcome message

--- server.js
+++ server.js
@@ -10,4 +10,11 @@ server.get("/", (req, res, next) => {
   })
 })
 
+server.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).json({
+    message: "Something is terribly wrong friend"
+  })
+})
+
 module.exports = server;
